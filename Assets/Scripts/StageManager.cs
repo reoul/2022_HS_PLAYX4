d97252e4b82636f8a81e845a4c0bdb32e383de34
G1Seok <91360038+G1Seok@users.noreply.github.com>
2022-03-20 20:42:45 +0900

    Default Project
    스페이바 입력시 플레이어 체력 감소
    오른쪽 방향키 입력시 스테이지 변경

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] stages;
+
+    private int currentStage = 1;
+
+    private void NextStage()
+    {
+        if(++currentStage > stages.Length)
+        {
+            currentStage = 1;
+        }
+        setupStage();
+    }
+
+    private void setupStage()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        Instantiate(stages[currentStage - 1],transform);
+    }
+
+    private void Update() //디버깅용 :: 오른쪽 화살표 버튼 입력시 다음 스테이지 세팅
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            NextStage();
+        }
+    }
+}
