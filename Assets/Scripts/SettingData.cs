b6133c0328cc56517c65ff2599b6525b1523bef3
reoul <dydqja1358@gmail.com>
2022-03-21 23:34:20 +0900

    SettingData 클래스 추가

---
+++ Assets/Scripts/SettingData.cs
@@ -0,0 +1,13 @@
+﻿public class SettingData
+{
+    public int PlayerHp;
+    public int BossHp;
+    public int BossDamage;
+
+    public SettingData()
+    {
+        PlayerHp = 100;
+        BossHp = 100;
+        BossDamage = 13;
+    }
+}
