58f7b18cb8f18a4f44177cf8b7d6866d7ff4bb7a
reoul <dydqja1358@gmail.com>
2022-03-21 22:01:21 +0900

    유니티 버전 2019.4.36으로 업그레이드

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    public void Save(string content)
+    {
+        string path = "";
+        var writer = new StreamWriter(path, false);
+        writer.Write(content);
+        writer.Flush();
+        writer.Close();
+    }
+
+    public void Load()
+    {
+        string path = "";
+        StreamReader reader = new StreamReader(path);
+        string content = reader.ReadToEnd();
+        reader.Close();
+
+        GameObject a = JsonUtility.FromJson<GameObject>(content);
+    }
+}
