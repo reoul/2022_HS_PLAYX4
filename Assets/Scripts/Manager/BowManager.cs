6e0df9a442534428e333909ef790832ae4d00433
reoul <dydqja1358@gmail.com>
2022-03-26 18:30:51 +0900

    수정

--- Assets/Scripts/Manager/BowManager.cs
+++ Assets/Scripts/Manager/BowManager.cs
@@ -1,5 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class BowManager : Singleton<BowManager>
 {
+    public LineRenderer _arrowTrajectoryLineRenderer;
+
+    private void Awake()
+    {
+        _arrowTrajectoryLineRenderer = GameObject.Find("ArrowTrajectoryLine").GetComponent<LineRenderer>();
+        _arrowTrajectoryLineRenderer.SetPosition(0,new Vector3(0.5f,1,0));
+        _arrowTrajectoryLineRenderer.SetPosition(1,new Vector3(0,1,5));
+    }
+
+    private void Update()
+    {
+        ShowArrowTrajectory();
+    }
+
+    /// <summary>
+    /// 화살의 궤적을 보여준다
+    /// </summary>
+    private void ShowArrowTrajectory()
+    {
+        _arrowTrajectoryLineRenderer.SetPosition(0,_arrowTrajectoryLineRenderer.GetPosition(0) + Vector3.forward * Time.deltaTime);
+        _arrowTrajectoryLineRenderer.SetPosition(1,_arrowTrajectoryLineRenderer.GetPosition(1) + Vector3.forward * Time.deltaTime);
+    }
+    
+    
 }
