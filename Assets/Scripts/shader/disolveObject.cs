c8d5054b9484d6ca828e603f6b1a33f68a4e679f
bykiwi99 <102608416+bykiwi99@users.noreply.github.com>
2022-03-30 17:54:46 +0900

    디졸브

---
+++ Assets/Scripts/shader/disolveObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disolveObject : MonoBehaviour
+{
+    [SerializeField] private float noiseStrength = 0.25f;
+    [SerializeField] private float objectHeight = 1.0f;
+
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        var time = Time.time * Mathf.PI * 0.5f;
+
+        float height = transform.position.y;
+        height += Mathf.Sin(time) * objectHeight * 8 ;
+        SetHeight(height);
+    }
+
+    private void SetHeight(float height)
+    {
+        material.SetFloat("_CutoffHeight", height);
+        material.SetFloat("_NoiseStrength", noiseStrength);
+    }
+}
