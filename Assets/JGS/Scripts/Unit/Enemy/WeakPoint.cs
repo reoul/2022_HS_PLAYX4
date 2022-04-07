a963a1a6f3bea1847521de1db62635e850223d32
GiSeok <jangisuk2@naver.com>
2022-04-07 20:15:25 +0900

    약점2

---
+++ Assets/JGS/Scripts/Unit/Enemy/WeakPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakPoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "arrow")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
