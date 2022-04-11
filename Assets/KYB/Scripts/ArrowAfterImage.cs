5c7a24b146fa21182fe2b3721f8ea6ccc5fa7adb
reoul <dydqja1358@gmail.com>
2022-04-11 14:02:27 +0900

    수정
    수정

--- Assets/KYB/Scripts/ArrowAfterImage.cs
+++ Assets/KYB/Scripts/ArrowAfterImage.cs
@@ -18,13 +18,33 @@ public class ArrowAfterImage : MonoBehaviour
 
     private void Update()
     {
-        if(_isShot)
+        Effect2();
+    }
+
+    private void Effect1()
+    {
+        if (_isShot)
         {
             _lineRenderer.SetPosition(1, _lineRenderer.GetPosition(1) + (_direction * _speed));
             _speed *= 100 * Time.deltaTime;
-            if(_speed >= 1000)
+            if (_speed >= 1000)
             {
                 _isShot = false;
+            }
+        }
+    }
+
+    private void Effect2()
+    {
+        if (_isShot)
+        {
+            _speed *= 1 + Time.deltaTime;
+            _lineRenderer.material.color -= Color.black * _speed * Time.deltaTime;
+            if (_lineRenderer.material.color.a <= 0)
+            {
+                _isShot = false;
+                _lineRenderer.SetPosition(0, Vector3.zero);
+                _lineRenderer.SetPosition(1, Vector3.zero);
             }
         }
     }
@@ -34,6 +54,7 @@ public class ArrowAfterImage : MonoBehaviour
         _speed = 1;
         _lineRenderer.SetPosition(0, VRControllerManager.Instance.Direction * 10000);
         _lineRenderer.SetPosition(1, VRControllerManager.Instance.BowController.transform.position);
+        _lineRenderer.material.color = Color.white;
         _direction = VRControllerManager.Instance.Direction.normalized;
         _isShot = true;
     }
