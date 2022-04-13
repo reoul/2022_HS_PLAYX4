d82fcacf4f44ce38352441835b5021afcf738e9c
G1Se0k <jangisuk2@naver.com>
2022-04-13 13:27:12 +0900

    골렘 애니메이션 수정

--- Assets/JGS/Scripts/Unit/Enemy/Golem/Golem.cs
+++ Assets/JGS/Scripts/Unit/Enemy/Golem/Golem.cs
@@ -8,20 +8,53 @@ public class Golem : Enemy
 
     private int _targetFloor;
 
+    private Vector3 _startPos;
+
+    private Vector3 _targetPos;
+
     private void Start()
     {
         _target = GameObject.Find("[CameraRig]").transform;
+        _startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        if (_isTargeted)
+        {
+            transform.position = Vector3.Lerp(transform.position, _targetPos, 0.05f);
+        }
+    }
+
+    private bool _isTargeted;
+
+    public void MoveToTarget()
+    {
+        _isTargeted = true;
+    }
+
+    public void SetTargetZero()
+    {
+        _targetPos = _startPos;
+        _isTargeted = true;
     }
 
     public void SetTargetFloor()
     {
         _targetFloor = Random.RandomRange(0, 3);
+        _targetPos = PlayerFloor.Instance.floorTransforms[_targetFloor].position + new Vector3(0, 0, 2);
         StartCoroutine(PlayerFloor.Instance.StartAttack(_targetFloor));
-        this.transform.position = PlayerFloor.Instance.floorTransforms[_targetFloor].position;
     }
 
     public void DisableTargetFloor()
     {
        PlayerFloor.Instance.StopAttack(_targetFloor);
+        StopTargeting();
     }
+
+    public void StopTargeting()
+    {
+        _isTargeted = false;
+    }
+    
 }
