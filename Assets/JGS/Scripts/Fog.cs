ac8c641be36c894871772959d62b25b071fc8a24
G1Seok <91360038+G1Seok@users.noreply.github.com>
2022-04-15 14:27:04 +0900

    Fog스크립트 사용
    아무 오브젝트에 넣고 GameStart() 함수 호출

---
+++ Assets/JGS/Scripts/Fog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog : MonoBehaviour
+{
+    private bool _isGameStart;
+    
+    // Use this for initialization
+    private void Start ()
+    {
+        _isGameStart = false;
+        
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = Color.black;
+        RenderSettings.fogMode = FogMode.Linear;
+        RenderSettings.fogStartDistance = 0;
+        RenderSettings.fogEndDistance = 5;
+
+    }
+
+    private void Update () {
+        if (_isGameStart)
+        {
+            RenderSettings.fogEndDistance = Mathf.Lerp(RenderSettings.fogEndDistance, 100, Time.deltaTime * 2.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GameStart();
+        }
+    }
+
+    public void GameStart()
+    {
+        _isGameStart = true;
+    }
+}
