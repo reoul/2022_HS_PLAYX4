7c77ffa7ea6d8926ab47a764a6270f5bbbee622a
WhiteWeb0707 <91812936+WhiteWeb0707@users.noreply.github.com>
2022-04-21 16:56:59 +0900

    골렘 사운드적용

---
+++ Assets/LSM/Scripts/Golem/Golem_MtJump.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem_MtJump : StateMachineBehaviour
+{
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SoundManager.Instance.PlaySoundThird("G_Hit_2", 1f);
+    }
+}
