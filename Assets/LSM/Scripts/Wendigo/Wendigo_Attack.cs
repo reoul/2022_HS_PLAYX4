3c5f3f73fc8e095ffcf298305121a38e5309c82e
WhiteWeb0707 <91812936+WhiteWeb0707@users.noreply.github.com>
2022-04-22 14:46:27 +0900

    웬디고 어택 사운드 추가

---
+++ Assets/LSM/Scripts/Wendigo/Wendigo_Attack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wendigo_Attack : StateMachineBehaviour
+{
+    float attackTime = 0;
+    bool attackCheck;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        attackCheck = true;
+    }
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        attackTime += Time.deltaTime;
+        if (attackTime >= 0.9f && attackCheck == true)
+        {
+            SoundManager.Instance.PlaySoundThird("Wendigo_Attack", 1f);
+            attackCheck = false;
+        }
+    }
+}
