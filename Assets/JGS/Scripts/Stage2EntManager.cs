b3ae6495d1147c59eb4bed985ecda203a2b06a8b
G1Se0k <jangisuk2@naver.com>
2022-04-22 10:59:31 +0900

    2스테이지

--- Assets/JGS/Scripts/Stage2EntManager.cs
+++ Assets/JGS/Scripts/Stage2EntManager.cs
@@ -7,11 +7,26 @@ public class Stage2EntManager : MonoBehaviour
     [SerializeField]
     private Transform[] _ents;
 
+    private float _attackDelay;
+    private float _lastAttackTime;
+
+    private void Start()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            _ents[i].GetComponent<JGS_Ent>().targetFloor = i;
+        }
+        _attackDelay = 5f;
+        _lastAttackTime = Time.time;
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        Debug.Log(Time.time - _lastAttackTime);
+        if (Time.time - _lastAttackTime > _attackDelay)
         {
             SetTarget();
+            _lastAttackTime = Time.time;
         }
     }
 
@@ -20,16 +35,18 @@ public class Stage2EntManager : MonoBehaviour
         int[] _randTaget = new int[2];
         _randTaget[0] = Random.RandomRange(0, 3);
         _randTaget[1] = _randTaget[0];
-        while (_randTaget[0] != _randTaget[1])
+        while (_randTaget[0] == _randTaget[1])
         {
             _randTaget[1] = Random.RandomRange(0, 3);
         }
         int _attackCount;
-        _attackCount = Random.RandomRange(0, 2);
+        _attackCount = Random.RandomRange(1, 3);
         for(int i= 0; i < _attackCount; i++)
         {
-            StartCoroutine(PlayerFloor.Instance.StartAttack(_randTaget[i]));
-            Debug.Log("pass");
+            Debug.Log(_ents[_randTaget[i]]);
+            _ents[_randTaget[i]].GetComponent<JGS_EntState>().Attack();
+            //StartCoroutine(PlayerFloor.Instance.StartAttack(_randTaget[i]));
+            //Debug.Log("pass");
         }
     }
 }
---
+++ Assets/JGS/Scripts/Unit/Enemy/JGS_Ent.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JGS_Ent : Enemy
+{
+    [SerializeField] private Transform _shootPosTransfrom;
+    [SerializeField] private GameObject _projectile;
+
+    private JGS_EntState _state;
+    private Vector3 _startPos;
+
+    public int targetFloor;
+
+    private void Start()
+    {
+        _state = this.GetComponent<JGS_EntState>();
+        _startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        //if(state == null)
+        //{
+        //    state = this.GetComponent<JGS_EntState>();
+        //}
+
+        //if (_state.IsIdle())
+        //{
+        //    transform.localPosition = Vector3.Lerp(transform.localPosition, _startPos, 0.1f);
+        //}
+    }
+
+    private GameObject _stone;
+
+    private void SpawnStone()
+    {
+        _stone = GameObject.Instantiate(_projectile);
+        _stone.transform.localPosition = Vector3.zero;
+        _stone.GetComponent<Projectile_Stone>()._rootTransform = _shootPosTransfrom;
+        _stone.GetComponent<Projectile_Stone>()._targetPos = PlayerFloor.Instance.attackTrans[targetFloor].position;
+        StartCoroutine(PlayerFloor.Instance.StartAttack(targetFloor));
+    }
+
+    public void ThrowStone()
+    {
+        _stone.GetComponent<Projectile_Stone>().isThrow = true;
+        _stone.GetComponent<Projectile_Stone>().targetFloor = targetFloor;
+    }
+}
