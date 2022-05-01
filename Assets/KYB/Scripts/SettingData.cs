7d53afbe70f745204c4abd1b0d5cb329f6d51baf
G1Seok <91360038+G1Seok@users.noreply.github.com>
2022-05-01 20:25:23 +0900

    SettingData 수정

--- Assets/KYB/Scripts/SettingData.cs
+++ Assets/KYB/Scripts/SettingData.cs
@@ -4,6 +4,24 @@
     public int BossHp;
     public int BossDamage;
     public float MoveSpeed;
+    public float FloorWidth;
+    
+    //====== Wendigo ======
+    public int WendigoMaxSpawnCount;
+    public float WendigoSpawnDelay;
+    public float WendigoMoveSpeed;
+    public int Stage1TargetScore;
+    public int Stage1TimeLimit;
+    public int WendigoDamage;
+    //====== Ent ======
+    public float EntAttackDelay;
+    public int Stage2TargetScore;
+    public int Stage2TimeLimit;
+    public int EntDamage;
+    //====== Golem ======
+    public int GolemMaxHealth;
+    public int Stage3TimeLimit;
+    public int GolemDamage;
 
     public SettingData()
     {
@@ -11,5 +29,19 @@
         BossHp = 100;
         BossDamage = 13;
         MoveSpeed = 100;
+        WendigoSpawnDelay = 0.5f;
+        WendigoMaxSpawnCount = 5;
+        WendigoMoveSpeed = 1f;
+        WendigoDamage = 100;
+        EntAttackDelay = 5f;
+        EntDamage = 100;
+        GolemMaxHealth = 20;
+        GolemDamage = 100;
+        FloorWidth = 1f;
+        Stage1TargetScore = 2000;
+        Stage2TargetScore = 4000;
+        Stage1TimeLimit = 10;
+        Stage2TimeLimit = 10;
+        Stage3TimeLimit = 10;
     }
 }
