d83096a95eaf00d278655c053ae48878f4ba740a
G1Se0k <jangisuk2@naver.com>
2022-05-05 17:04:05 +0900

    수정

--- Assets/JGS/Scripts/UI/HealthBarManager.cs
+++ Assets/JGS/Scripts/UI/HealthBarManager.cs
@@ -13,6 +13,8 @@ public class HealthBarManager : Singleton<HealthBarManager>
 
     private Stack<GameObject> _healthStack;
 
+    private Color[] color;
+
     private void Awake()
     {
         _healthStack = new Stack<GameObject>();
@@ -20,11 +22,14 @@ public class HealthBarManager : Singleton<HealthBarManager>
 
     private void Start()
     {
+        color = new Color[2] { new Color(0.9960784f, 0.3035352f, 0.282353f), new Color(0.282353f, 0.5973283f, 0.9960784f) };
+
         _maxHealth = DataManager.Instance.Data.GolemMaxHealth;
         for (int i = 0; i < _maxHealth; i++)
         {
             GameObject _tempHealthBlock = GameObject.Instantiate(_healthBlock, this.transform);
             RectTransform _tempRect = _tempHealthBlock.GetComponent<RectTransform>();
+            _tempHealthBlock.GetComponent<Image>().color = color[1];
             _tempRect.sizeDelta = new Vector2((20f / _maxHealth) * 65, _tempRect.sizeDelta.y);
             _tempHealthBlock.transform.localPosition = new Vector3(i * _tempRect.sizeDelta.x, 0, 0);
             _healthStack.Push(_tempHealthBlock);
@@ -36,14 +41,20 @@ public class HealthBarManager : Singleton<HealthBarManager>
         if (Input.GetKeyDown(KeyCode.Space)){
             if(_healthStack.Count > 0)
             {
-                GameObject _curHealthBlock = _healthStack.Pop();
-                Destroy(_curHealthBlock);
+                DistractDamage();
             }
         }
     }
 
     public void DistractDamage()
     {
-        Destroy(_healthStack.Pop());
+        if(_healthStack.Peek().GetComponent<Image>().color == color[1])
+        {
+            _healthStack.Peek().GetComponent<Image>().color = color[0];
+        }
+        else
+        {
+            Destroy(_healthStack.Pop());
+        }
     }
 }
