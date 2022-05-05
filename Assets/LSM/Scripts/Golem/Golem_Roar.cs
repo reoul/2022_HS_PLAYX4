8b28f2f970930e4947dcdecfb93796e4e3ef936f
WhiteWeb0707 <91812936+WhiteWeb0707@users.noreply.github.com>
2022-05-05 16:45:10 +0900

    수정

--- Assets/LSM/Scripts/Golem/Golem_Roar.cs
+++ Assets/LSM/Scripts/Golem/Golem_Roar.cs
@@ -8,7 +8,7 @@ public class Golem_Roar : StateMachineBehaviour
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        SoundManager.Instance.PlaySoundThird("monster-whirr-SBA-300063842-preview", 1f);
+        SoundManager.Instance.PlaySoundThird("GolemRoar", 1f);
     }
    
 }
