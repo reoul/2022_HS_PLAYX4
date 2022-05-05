f41f6efbe37d9e5041d7d01e6b2d4f7205025dbe
reoul <dydqja1358@gmail.com>
2022-05-05 18:30:18 +0900

    나레이션 시스템 추가
    나레이션 시스템 추가

---
+++ Assets/KYB/Scripts/NarrationManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class NarrationManager : Singleton<NarrationManager>
+{
+    public AudioSource AudioSource;
+    public AudioClip[] clips;
+    private int curIndex = -1;
+    private void Update()
+    {
+        if (VRControllerManager.Instance.BowController.GetTrigger() &&
+            VRControllerManager.Instance.ArrowController.GetTriggerDown())
+        {
+            PlayNextVoice();
+        }
+    }
+
+    private void PlayVoice()
+    {
+        AudioSource.Play();
+    }
+
+    private void PlayNextVoice()
+    {
+        AudioSource.clip = clips[++curIndex];
+        AudioSource.Play();
+    }
+}
