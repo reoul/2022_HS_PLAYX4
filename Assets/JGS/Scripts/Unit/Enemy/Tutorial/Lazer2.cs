4f1d773717c7c93158ca635df5d32075a6c7a8ab
G1Se0k <jangisuk2@naver.com>
2022-06-03 14:36:43 +0900

    추가

---
+++ Assets/JGS/Scripts/Unit/Enemy/Tutorial/Lazer2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazer2 : MonoBehaviour
+{
+    private Transform _lazer;
+
+
+    void Start()
+    {
+        Destroy(this.gameObject, 0.75f);
+        _lazer = this.transform.GetChild(0);
+    }
+     
+    void Update()
+    {
+        if (_lazer.localScale.y < 30)
+        {
+            _lazer.position -= _lazer.transform.up;
+            _lazer.localScale += new Vector3(0, 1, 0);
+        }
+        else
+        {
+            this.transform.rotation *= Quaternion.Euler(0, 0.5f, 0);
+        }
+    }
+
+
+}
